c91d8632f8a3d19844a59715fb4c4774d2f86e09
n-nazuna <negicyuu@gmail.com>
2026-08-28 22:03:40 +0900

    gpl macro追加

--- src/ata.rs
+++ src/ata.rs
@@ -1,84 +1,65 @@
-use crate::sg_io::{XferParam, XferDirection, XferLength};
+use crate::sg_io::{XferDirection, XferLength, XferParam};
 pub trait Ata {
-    fn command(&self) -> u8;
-    fn feature(&self) -> u16;
-    fn count(&self) -> u16;
-    fn lba(&self) -> u64;
-    fn icc(&self) -> u8;
-    fn aux(&self) -> u16;
-    fn device(&self) -> u8;
+    fn command(&self) -> u8 {
+        0
+    }
+    fn feature(&self) -> u16 {
+        0
+    }
+    fn count(&self) -> u16 {
+        0
+    }
+    fn lba(&self) -> u64 {
+        0
+    }
+    fn icc(&self) -> u8 {
+        0
+    }
+    fn aux(&self) -> u16 {
+        0
+    }
+    fn device(&self) -> u8 {
+        0
+    }
     fn xfer_length(&self) -> XferParam;
 }
-pub struct ReadLogExt {
-    pub log_page_count: u16,
-    pub log_address: u8,
-    pub page_number: u16,
-}
-impl Ata for ReadLogExt {
-    fn command(&self) -> u8 {
-        0x2F
-    }
-    fn feature(&self) -> u16 {
-        0
-    }
-    fn count(&self) -> u16 {
-        self.log_page_count
-    }
-    fn lba(&self) -> u64 {
-        ((self.page_number as u64 & 0xFF00) << 24)
-            | ((self.page_number as u64 & 0xFF) << 8)
-            | (self.log_address as u64)
-    }
-    fn icc(&self) -> u8 {
-        0
-    }
-    fn aux(&self) -> u16 {
-        0
-    }
-    fn device(&self) -> u8 {
-        0
-    }
-    fn xfer_length(&self) -> XferParam {
-        XferParam {
-            direction: crate::sg_io::XferDirection::TargetToInitiator,
-            length: crate::sg_io::XferLength::Pages(self.log_page_count as u32),
+macro_rules! define_gpl {
+    ($name:ident, $cmd:expr, $dir:expr) => {
+        pub struct $name {
+            pub log_page_count: u16,
+            pub log_address: u8,
+            pub page_number: u16,
         }
-    }
-}
-
-pub struct WriteLogExt {
-    pub log_page_count: u16,
-    pub log_address: u8,
-    pub page_number: u16,
-}
-impl Ata for WriteLogExt {
-    fn command(&self) -> u8 {
-        0x3F
-    }
-    fn feature(&self) -> u16 {
-        0
-    }
-    fn count(&self) -> u16 {
-        self.log_page_count
-    }
-    fn lba(&self) -> u64 {
-        ((self.page_number as u64 & 0xFF00) << 24)
-            | ((self.page_number as u64 & 0xFF) << 8)
-            | (self.log_address as u64)
-    }
-    fn icc(&self) -> u8 {
-        0
-    }
-    fn aux(&self) -> u16 {
-        0
-    }
-    fn device(&self) -> u8 {
-        0
-    }
-    fn xfer_length(&self) -> XferParam {
-        XferParam {
-            direction: crate::sg_io::XferDirection::InitiatorToTarget,
-            length: crate::sg_io::XferLength::Pages(self.log_page_count as u32),
+        impl $name {
+            fn page_number_hi(&self) -> u8 {
+                (self.page_number >> 8) as u8
+            }
+            fn page_number_lo(&self) -> u8 {
+                (self.page_number & 0xFF) as u8
+            }
         }
-    }
+        impl Ata for $name {
+            fn command(&self) -> u8 {
+                $cmd
+            }
+            fn count(&self) -> u16 {
+                self.log_page_count
+            }
+            fn lba(&self) -> u64 {
+                ((self.page_number_hi() as u64) << 32)
+                    | ((self.page_number_lo() as u64) << 8)
+                    | (self.log_address as u64)
+            }
+            fn xfer_length(&self) -> XferParam {
+                XferParam {
+                    direction: $dir,
+                    length: XferLength::Pages(self.log_page_count as u32),
+                }
+            }
+        }
+    };
 }
+define_gpl!(ReadLogExt, 0x2F, XferDirection::TargetToInitiator);
+define_gpl!(ReadLogDmaExt, 0x47, XferDirection::TargetToInitiator);
+define_gpl!(WriteLogExt, 0x3F, XferDirection::InitiatorToTarget);
+define_gpl!(WriteLogDmaExt, 0x57, XferDirection::InitiatorToTarget);
